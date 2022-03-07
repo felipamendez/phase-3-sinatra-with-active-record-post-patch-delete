af15e78e8d7bffe96bc245eafe216ec505c31e77
Felipa Mendez <felipalouise@gmail.com>
2022-03-07 14:28:23 -0600

    commit tree adebe04e345cd185a62f114c27b5dfd74395c2cd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,32 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+
+    delete '/reviews/:id' do
+      review = Review.find(params[:id])
+      review.destroy
+      review.to_json
+    end
+
+    post '/reviews' do
+      review = Review.create(
+        score: params[:score],
+        comment: params[:comment],
+        game_id: params[:game_id],
+        user_id: params[:user_id]
+      )
+      review.to_json
+    end
+
+    patch '/reviews/:id' do
+      review = Review.find(params[:id])
+      review.update(
+        score: params[:score],
+        somment: params[:comment]
+      )
+      review.to_json
+    end
+
   end
 
 end
